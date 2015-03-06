ad8bfd832018c92103656f2ed73e621cd72a337f
davidkuep <davidkohn@davids-mbp.home>
2015-03-05 19:33:50 -0500

    Changed to Function

--- mysqrt.py
+++ mysqrt.py
@@ -4,20 +4,33 @@ Created on Wed Mar  4 17:41:27 2015
 
 @author: davidkohn
 """
+def sqrt2(x):
+    s = x/2
+    tol = 1.e-14
+    kmax=10000
+    for k in range(kmax) :
+        print "Before iteration %s, s = %s" %(k,s)
+        s0 = s
+        s = 0.5 * (s+x/s)
+        k = k+1
+        
+        if abs((s-s0)/s)<tol:
+            break
+        
+    print "After %s iterations s = %s" %(k,s)
 
-x = 0.1
-s = x/2
-tol = 1.e-14
-kmax=10000
-for k in range(kmax) :
-    print "Before iteration %s, s = %s" %(k,s)
-    s0 = s
-    s = 0.5 * (s+x/s)
-    k = k+1
-    
-    if abs((s-s0)/s)<tol:
-        break
-    
 
-print "After %s iterations s = %s" %(k,s)
-
+def sqrt3(x):
+    s = 1.
+    tol = 1.e-14
+    kmax=10000
+    for k in range(kmax) :
+        print "Before iteration %s, s = %s" %(k,s)
+        s0 = s
+        s = 0.5 * (s+x/s)
+        k = k+1
+        
+        if abs((s-s0)/s)<tol:
+            break
+        
+    print "After %s iterations s = %s" %(k,s)
